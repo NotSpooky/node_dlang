93b3826d3e5fe7e83059dc7501b3318b3b4802bd
Christian Durán <cduran@symbiotic.net>
2020-07-28 18:17:09 -0600

    Change linux binding.gyp filename

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+cp linux_binding.gyp binding.gyp
 ldc src/example.d src/dlang_node.d src/node_api.d src/js_native_api.d src/js_native_api_types.d src/node_version.d --output-s -od src/ &&
   node-gyp configure build && 
   mv build/Release/module.node ~/Programming/wasmgame-electron/ 
