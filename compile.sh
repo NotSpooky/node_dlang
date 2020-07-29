fcfcb0ca186843e8593fd380434c0a24b2b5c6ed
Christian Durán <cduran@symbiotic.net>
2020-07-28 18:25:52 -0600

    Don't use assembly files

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 cp linux_binding.gyp binding.gyp
-ldc src/example.d src/dlang_node.d src/node_api.d src/js_native_api.d src/js_native_api_types.d src/node_version.d --output-s -od src/ &&
+ldc src/example.d src/dlang_node.d src/node_api.d src/js_native_api.d src/js_native_api_types.d src/node_version.d --lib -of src/example.a &&
   node-gyp configure build && 
   mv build/Release/module.node ~/Programming/wasmgame-electron/ 
--- linux_binding.gyp
+++ linux_binding.gyp
@@ -1,10 +1,11 @@
 {
     "targets": [{
         "target_name": "module",
-        "sources": [ "./src/example.s", "./src/dlang_node.s", "./src/module.c" ],
+        "sources": [ "./src/module.c" ],
         "libraries": [
           "/usr/lib/libphobos2-ldc-debug.a",
-          "/usr/lib/libdruntime-ldc-debug.a"
+          "/usr/lib/libdruntime-ldc-debug.a",
+          "$(PWD)/src/example.a"
         ]
     }]
 }
