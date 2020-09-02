b02b66b30082cda8974c875265aeababf8ed3789
Christian Durán <cduran@symbiotic.net>
2020-09-01 19:09:58 -0600

    Delete example

--- examples/example/example.js
+++
@@ -1,5 +0,0 @@
-const mymodule = require ('./module.node');
-console.log ('Calling fumiko:');
-console.log ('Got result: ', mymodule.fumiko());
-console.log ('Calling logExample');
-mymodule.logExample (console);
